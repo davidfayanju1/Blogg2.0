3bf0241bd5f95092270c27e22cf7ec814691edae
davidfayanju1 <bayo@gmail.com>
2022-08-20 09:02:53 +0100

    blog Post page setup

--- src/pages/NewBlog.js
+++ src/pages/NewBlog.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
 import { BsPlusCircle } from 'react-icons/bs';
@@ -11,6 +11,7 @@ const NewBlog = () => {
     const user = {
         name: 'Billie Will',
     }
+
     const [ open, setOpen ] = useState(false);
     const [ blogData, setBlogData ] = useState({})
 
@@ -20,6 +21,38 @@ const NewBlog = () => {
         setOpen(true);
 
     }
+
+    const titleRef = useRef();
+    const bodyRef = useRef();
+
+    const [ newBlog, setNewBlog ] = useState(false);
+
+    const postBlog = () => {
+
+
+        console.log({
+            title:titleRef.current.value,
+            body: bodyRef.current.value 
+        })
+
+        
+
+    }
+
+    const updatePostBtn = (e) => {
+
+
+        if(e.target.value){
+            
+            setNewBlog(true);
+
+        }
+
+
+    }
+
+    const { register, getValues } = useForm();
+
     
   return (
     <div className='new-blog min-h-[100vh] dark:bg-slate-800 bg-white dark:text-white'>
@@ -30,7 +63,7 @@ const NewBlog = () => {
                 </Link>
             </div>
             <div className="newblog-controls flex items-center gap-[1rem]">
-                <button className='bg-green-800 hover:bg-green-900 h-[2rem] w-[6rem] text-white font-semibold rounded-[20px]'>Publish</button>
+                <button className='bg-green-800 hover:bg-green-900 h-[2rem] w-[6rem] text-white font-semibold rounded-[20px] disabled:opacity-[.6]' disabled={ !newBlog } onClick={ postBlog }>Publish</button>
                 <div className="user">
                     {user.img ? <img src={user.img} alt="user" /> : <p className='bg-red-800 w-[2.2rem] h-[2.2rem] rounded-[100%] text-[1.3rem] flex items-center justify-center font-semibold text-white'> { user.name.charAt(0) }</p>}
                 </div>
@@ -40,7 +73,7 @@ const NewBlog = () => {
         <div className="blog-post-body md:px-[22rem] mt-[1.5rem] px-[2.3rem]">
             <form onSubmit={ openInput }>
                 <div className="form-group mb-[.75rem]">
-                    <input type="text"  autoFocus className='w-[100%] dark:bg-slate-800 h-[4.5rem] placeholder:font-serif md:placeholder:text-[2.6rem] placeholder:text-[1.8rem] md:text-[2.6rem] text-[1.8rem] outline-none border-none font-serif text-gray-700 dark:text-white' placeholder='Title'/>
+                    <input type="text" name='title' onChange = { updatePostBtn } autoFocus  ref={ titleRef } className='w-[100%] dark:bg-slate-800 h-[4.5rem] placeholder:font-serif md:placeholder:text-[2.6rem] placeholder:text-[1.8rem] md:text-[2.6rem] text-[1.8rem] outline-none border-none font-serif text-gray-700 dark:text-white' placeholder='Title' required/>
                 </div>
 
                 {
@@ -48,7 +81,7 @@ const NewBlog = () => {
 
                     <div className="form-group">
                         <div className="container flex items-center relative">
-                            <BsPlusCircle className="md:text-[2rem] text-[1.4rem] absolute top-[0%] md:top-[0%] left-[-7%] font-extralight text-gray-800 cursor-pointer dark:text-white" title="Upload Image"/><textarea type="text"  className='resize-none dark:text-gray-300 dark:bg-slate-800 w-[100%] min-h-[70vh] overflow-hidden overflow-y-auto text-gray-700 placeholder:font-serif md:placeholder:text-[1.29rem] placeholder:text-[1.1rem] md:text-[1.29rem] text-[1.1rem] outline-none border-none font-serif' placeholder='Tell your story...'></textarea>
+                            <BsPlusCircle className="md:text-[2rem] text-[1.4rem] absolute top-[0%] md:top-[0%] left-[-7%] font-extralight text-gray-800 cursor-pointer dark:text-white" title="Upload Image"/><textarea type="text" ref={bodyRef} name='body' className='resize-none dark:text-gray-300 dark:bg-slate-800 w-[100%] min-h-[70vh] overflow-hidden overflow-y-auto text-gray-700 placeholder:font-serif md:placeholder:text-[1.29rem] placeholder:text-[1.1rem] md:text-[1.29rem] text-[1.1rem] outline-none border-none font-serif' placeholder='Tell your story...'></textarea>
                         </div>
                     </div>
                 }
